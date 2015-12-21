b0c83ceddd4a9ff53851f6dc31f8f63a57693819
Adrian <adii_1989@tlen.pl>
2015-12-21 14:35:09 +0100

    Klasa pionkow

--- Pionek.java
+++ Pionek.java
@@ -2,4 +2,32 @@ package test;
 
 public class Pionek {
 
+	//Opisuje do ktorego gracza nalezy dany pionek ( 1-4 )
+	int idGracza;
+	
+	//Opisuje ktory to pionek danego gracza ( 1-4 )
+	int idPionka;
+	
+	public int getIdGracza() {
+		return idGracza;
+	}
+
+	public void setIdGracza(int idGracza) {
+		this.idGracza = idGracza;
+	}
+
+	public int getIdPionka() {
+		return idPionka;
+	}
+
+	public void setIdPionka(int idPionka) {
+		this.idPionka = idPionka;
+	}
+
+	
+	
+	Pionek(){}
+	
+	
+	
 }
